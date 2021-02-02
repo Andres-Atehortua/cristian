a1acfe50c89733fe2e2bdb67dab02aea8e4dfd1d
Andrés Felipe López Atehortua <alopez@almacorp.es>
2021-02-02 19:32:53 +0100

    asdasd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect } from 'react';
+import './styles.css';
 function App() {
+  useEffect(() => {
+    const banner = document.querySelector('.banner');
+    const blocks = document.getElementsByClassName('blocks');
+
+    for (let i = 0; i < 100; i++) {
+      banner.innerHTML += '<div class="blocks"></div>';
+      const duration = Math.random() * 5;
+      blocks[i].style.animationDuration = duration + 's';
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className='App'>
+        <div className='banner'></div>
+      </section>
+    </>
   );
 }
 
